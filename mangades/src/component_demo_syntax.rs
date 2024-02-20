8a63a219c50a658fc9050765f7434035f3d7ccb7
Daniel Bulant <danbulant@gmail.com>
2024-02-20 22:22:24 +0100

    image demo

--- mangades/src/component_demo_syntax.rs
+++ mangades/src/component_demo_syntax.rs
@@ -1,6 +1,6 @@
 use rusalka_macro::make_component;
 use std::default::Default;
-use mangui::{SharedNode, nodes::Style, taffy::prelude::Size, femtovg::{Paint, Color}, nodes::layout::Layout};
+use mangui::{femtovg::ImageFlags, nodes::{layout::Layout, Style}, nodes::image::Image, taffy::prelude::Size};
 
 use rusalka::nodes::primitives::{Rectangle, RectangleAttributes, PartialRectangleAttributes};
 
@@ -8,6 +8,10 @@ make_component!(
     ComponentDemo,
     MainLogic {
         let _radius = attrs.radius;
+        let imgpath = std::path::PathBuf::from("./demo/large/bx117324-97mHyfJGwpBq.jpg");
+        let imgflags = ImageFlags::empty();
+        let width = 230.;
+        let height = 325.;
     }
     Attributes {
         radius: f32
@@ -23,6 +27,24 @@ make_component!(
             @Rectangle {
                 radius: if $test_ { attrs.radius } else { 0. },
                 ..Default::default()
+            }
+            @image {
+                style: Style {
+                    layout: mangui::nodes::TaffyStyle {
+                        min_size: Size {
+                            width: mangui::taffy::style::Dimension::Points(width),
+                            height: mangui::taffy::style::Dimension::Points(height)
+                        },
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                image: mangui::nodes::image::ImageLoad::LoadFile(imgpath, imgflags),
+                width,
+                height,
+                radius: 5.,
+                events: Default::default(),
+                parent: None
             }
             $|event| {
                 match event.event {
